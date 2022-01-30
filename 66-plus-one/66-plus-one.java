13f7b5e2edbcf81052c35738a4d32ec3ff04a026
Aqib Altaf <61707240+aqibaltaf@users.noreply.github.com>
2022-01-30 13:23:36 +0500

    Time: 1 ms (19.57%), Space: 42.2 MB (5.47%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,30 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        digits[digits.length - 1] = digits[digits.length - 1] + 1;
-        int count = 1;
-        int[] arr = new int[digits.length + 1];
-        if (digits[digits.length - 1] == 10) {
-            digits[digits.length - 1] = 0;
-            for (int i = digits.length - 2; i >= 0; i--) {
-
-                if (digits[i] != 9) {
-                    digits[i] = digits[i] + 1;
-                    break;
-
-                } else {
-                    digits[i] = (digits[i] + 1) % 10;
-                    count++;
-                }
-            }
-            if (digits[0] == 0 && count == digits.length) {
-                arr[0] = 1;
-                for (int j = 1; j < digits.length; j++) {
-                    arr[j] = digits[j];
-                }
-                  digits = arr;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            } else {
+                digits[i] = 0;
             }
         }
 
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        
         return digits;
     }
 }
